2e3996763972e04e578cafd7b356f578957ce449
Esko Luontola <esko@luontola.fi>
2020-01-14 12:30:32 +0200

    Build our own Docker image for Jekyll
    Why:
    - starefossen/github-pages has an out-of-date bundler
    - By using the same Gemfile, the Jekyll environment will
      be the same regardless of whether using Docker or running
      Jekyll natively

--- serve.sh
+++ serve.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eux
-docker run -it --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+docker build -t codefreeze-jekyll .
+docker run -it --rm -v "$PWD":/project -p "4000:4000" codefreeze-jekyll
